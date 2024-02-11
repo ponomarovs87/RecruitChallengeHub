f67fa97c6a650d52088b3d05830be0728659e048
ponomarovs87 <ponomarovs87@gmail.com>
2024-02-11 02:58:48 +0100

    fist stip add html fullfild

---
+++ 20240105APsiteWebStudio/dist/script.js
@@ -0,0 +1,6 @@
+console.log("Hello from script.js!");
+console.log("hi");
+console.log("div");
+const vdndfskjnbjks = 1;
+const jvbjd = 2;
+const filwe = hjbsvhjdsbkjgbv;
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RecruitChallengeHub
+This repository only for test homework from Requters All in one
