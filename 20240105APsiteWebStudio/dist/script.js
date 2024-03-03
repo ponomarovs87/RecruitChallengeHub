e4fce5aa15656e3f594f3901ef89b5d56c235aec
ponomarovs87 <ponomarovs87@gmail.com>
2024-03-03 19:44:56 +0100

    reedit errors :-(

--- 20240105APsiteWebStudio/dist/script.js
+++ 20240105APsiteWebStudio/dist/script.js
@@ -2,6 +2,20 @@ window.onscroll = function () {
   scrollFunction();
 };
 
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", scrollUpShow, false);
+
+function scrollUpShow() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    document.querySelector(".shoppingButtons").classList.add("hide");
+  } else {
+    document.querySelector(".shoppingButtons").classList.remove("hide");
+  }
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+}
+
 function scrollFunction() {
   const winScroll =
     document.body.scrollTop || document.documentElement.scrollTop;
